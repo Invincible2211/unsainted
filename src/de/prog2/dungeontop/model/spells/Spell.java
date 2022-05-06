6d1544713b466f732fe2a3e2b7a3fdb3a41ebd33
jpranata <jpranata@stud.hs-bremen.de>
2022-05-06 18:36:24 +0200

    Spells angefangen

--- src/de/prog2/dungeontop/model/spells/Spell.java
+++ src/de/prog2/dungeontop/model/spells/Spell.java
@@ -1,4 +1,43 @@
 package de.prog2.dungeontop.model.spells;
 
-public abstract class Spell {
+public abstract class Spell
+{
+    protected String name;
+
+    protected int mana_cost;
+
+    // definitely incomplete, maybe damage and heal = changes in health instead?
+    protected int damage;
+    protected int heal;
+    protected int draw_card;
+
+
+    protected abstract void effekt();
+
+    public Spell (String name, int mana_cost)
+    {
+        this.name = name;
+        this.mana_cost = mana_cost;
+    }
+
+    //Set- and Getters
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getMC()
+    {
+        return mana_cost;
+    }
+
+    public void setMC(int mana_cost)
+    {
+        this.mana_cost = mana_cost;
+    }
+
 }
