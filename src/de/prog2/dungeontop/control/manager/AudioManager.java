4e88ac4edcdeea2f58fb0aa4f689b1a616410f8b
Freddyblitz <fynn.schur@web.de>
2022-05-08 18:33:43 +0200

    grobe implementierung der Managerklassen -> warte auf implementierung von Abhaenigkeiten

--- src/de/prog2/dungeontop/control/manager/AudioManager.java
+++ src/de/prog2/dungeontop/control/manager/AudioManager.java
@@ -2,6 +2,12 @@ package de.prog2.dungeontop.control.manager;
 
 public class AudioManager {
 
+    public static void playSound(int soundID){
 
+    }
+
+    public static void stopAudio(){
+
+    }
 
 }
--- src/de/prog2/dungeontop/control/manager/BattleManager.java
+++ src/de/prog2/dungeontop/control/manager/BattleManager.java
@@ -1,4 +1,50 @@
 package de.prog2.dungeontop.control.manager;
 
+import de.prog2.dungeontop.model.entities.Entity;
+import de.prog2.dungeontop.model.game.DungeonMaster;
+import de.prog2.dungeontop.model.game.GameState;
+import de.prog2.dungeontop.model.game.Player;
+
 public class BattleManager {
+
+    public void startBattle(){
+
+    }
+
+    public void endBattle(){
+
+    }
+
+    public void nextPhase(){
+
+    }
+
+    public GameState getPhase(){
+
+    }
+
+    public void startRound(){
+
+    }
+
+    public void placeCard(Player player){
+
+    }
+
+    public void placeCard(DungeonMaster dungeonMaster){
+
+    }
+
+    public void attack(Player player){
+
+    }
+
+    public void attack(DungeonMaster dungeonMaster){
+
+    }
+
+    public void endRound(){
+
+    }
+
 }
