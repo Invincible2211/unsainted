33179ad97ea886a9f476ac47d0f01eca2f939137
Jesse Gollub <jesse.gollub@ewe.net>
2022-05-13 11:43:41 +0200

    Added the suggested rooms

--- src/de/prog2/dungeontop/model/world/Room.java
+++ src/de/prog2/dungeontop/model/world/Room.java
@@ -1,7 +1,9 @@
 package de.prog2.dungeontop.model.world;
 
-public abstract class Room extends HellComponent
+public abstract class Room
 {
+    private Room topRoom, bottomRoom, leftRoom, rightRoom;
+    
     private Action action = new DefaultAction();
 
     public Room(){}
@@ -13,5 +15,46 @@ public abstract class Room extends HellComponent
     public <T> boolean executeAction(T... args)
     {
         return this.action.execute(args);
+    }
+
+    /*-----------------------------------------GETTER AND SETTER------------------------------------------------------*/
+    public Room getTopRoom()
+    {
+        return topRoom;
+    }
+
+    public Room getBottomRoom()
+    {
+        return bottomRoom;
+    }
+
+    public Room getLeftRoom()
+    {
+        return leftRoom;
+    }
+
+    public Room getRightRoom()
+    {
+        return rightRoom;
+    }
+
+    public void setTopRoom(Room topRoom)
+    {
+        this.topRoom = topRoom;
+    }
+
+    public void setBottomRoom(Room bottomRoom)
+    {
+        this.bottomRoom = bottomRoom;
+    }
+
+    public void setLeftRoom(Room leftRoom)
+    {
+        this.leftRoom = leftRoom;
+    }
+
+    public void setRightRoom(Room rightRoom)
+    {
+        this.rightRoom = rightRoom;
     }
 }
