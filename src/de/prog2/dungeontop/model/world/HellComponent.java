c30da41cbcda6025a8c829e2a7b0c3988c23822d
Jesse Gollub <jesse.gollub@ewe.net>
2022-05-13 11:47:43 +0200

    added Rotation-Attribute to HellComponent and added the attributes from the deprecated Cell to HellComponent

--- src/de/prog2/dungeontop/model/world/HellComponent.java
+++ src/de/prog2/dungeontop/model/world/HellComponent.java
@@ -2,5 +2,15 @@ package de.prog2.dungeontop.model.world;
 
 public abstract class HellComponent
 {
-
+    Rotation rotation = Rotation.UP;
+    private boolean isVisible = false;
+    /*-----------------------------------------GETTER AND SETTER------------------------------------------------------*/
+    public boolean isVisible()
+    {
+        return isVisible;
+    }
+    public void setVisible(boolean visible)
+    {
+        isVisible = visible;
+    }
 }
---
+++ src/de/prog2/dungeontop/model/world/RoomCenter.java
@@ -0,0 +1,10 @@
+package de.prog2.dungeontop.model.world;
+
+public class RoomCenter extends HellComponent
+{
+    private Room room;
+    public RoomCenter(Room room)
+    {
+        this.room = room;
+    }
+}
