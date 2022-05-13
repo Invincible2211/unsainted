ccc43806fcf2cad4f979fd803708164e2c169abf
Jesse Gollub <jesse.gollub@ewe.net>
2022-05-13 18:41:37 +0200

    added Asset id to HellComponent and created UUIDs for the different HellComponents inside WorldConstants

--- src/de/prog2/dungeontop/model/world/Passage.java
+++ src/de/prog2/dungeontop/model/world/Passage.java
@@ -1,5 +1,11 @@
 package de.prog2.dungeontop.model.world;
 
+import de.prog2.dungeontop.resources.WorldConstants;
+
 public class Passage extends HellComponent
 {
+    public Passage(Rotation rotation)
+    {
+        super(WorldConstants.RoomAssetIds.PASSAGE_ASSET_ID, rotation);
+    }
 }
--- src/de/prog2/dungeontop/resources/WorldConstants.java
+++ src/de/prog2/dungeontop/resources/WorldConstants.java
@@ -1,5 +1,7 @@
 package de.prog2.dungeontop.resources;
 
+import java.util.UUID;
+
 public interface WorldConstants
 {
     /**
@@ -9,4 +11,12 @@ public interface WorldConstants
     int HELL_SIZE = 7;
     int LOWEST_COORDINATE = 0;
     int RANDOMIZER_LIMIT = 6;
+
+    public interface RoomAssetIds
+    {
+        UUID WALL_ASSET_ID = UUID.randomUUID();
+        UUID WALL_CORNER_ASSET_ID = UUID.randomUUID();
+        UUID PASSAGE_ASSET_ID = UUID.randomUUID();
+        UUID ROOM_CENTER_ASSET_ID = UUID.randomUUID();
+    }
 }
