ad97510db04d4f8f2a7f6be1d84dc4a88427b0e2
jpranata <jpranata@stud.hs-bremen.de>
2022-05-14 19:40:29 +0200

    Artifact and Weapon now extends Item. Constructors for Artifact and Weapon added.

--- src/de/prog2/dungeontop/model/items/Artifact.java
+++ src/de/prog2/dungeontop/model/items/Artifact.java
@@ -1,4 +1,14 @@
 package de.prog2.dungeontop.model.items;
 
-public abstract class Artifact {
+public abstract class Artifact extends Item
+{
+    public Artifact(String name, int price)
+    {
+        super(name, price);
+    }
+
+    public Artifact(String name, int price, int bonus)
+    {
+        super(name, price, bonus);
+    }
 }
