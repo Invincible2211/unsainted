4125ec38cb63f2b0c35c4ad1f068a35b07b1ce3e
jpranata <jpranata@stud.hs-bremen.de>
2022-05-14 18:47:16 +0200

    added ego points and deck, and their get- and setters

--- src/de/prog2/dungeontop/model/game/DungeonMaster.java
+++ src/de/prog2/dungeontop/model/game/DungeonMaster.java
@@ -1,4 +1,32 @@
 package de.prog2.dungeontop.model.game;
 
-public class DungeonMaster {
+public class DungeonMaster
+{
+    private int ego_points;
+    private Deck deck;
+
+    public DungeonMaster()
+    {
+
+    }
+    
+    //Set- and Getters
+    public int getEgo_points()
+    {
+        return ego_points;
+    }
+    public void setEgo_points(int ego_points)
+    {
+        this.ego_points = ego_points;
+    }
+
+    public Deck getDeck()
+    {
+        return deck;
+    }
+
+    public void setDeck(Deck deck)
+    {
+        this.deck = deck;
+    }
 }
