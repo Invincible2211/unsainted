10db1a259526bca89efb221882673804e29a8b4d
Jesse Gollub <jesse.gollub@ewe.net>
2022-05-14 16:35:54 +0200

    added PlayerMovement

--- src/de/prog2/dungeontop/control/manager/MovementManager.java
+++ src/de/prog2/dungeontop/control/manager/MovementManager.java
@@ -1,4 +1,67 @@
 package de.prog2.dungeontop.control.manager;
 
-public class MovementManager {
+import de.prog2.dungeontop.model.game.MoveDirection;
+import de.prog2.dungeontop.model.game.Player;
+
+public class MovementManager
+{
+    //prüfen laufen, movent smooth
+    public static final MovementManager instance = new MovementManager();
+
+    public boolean moveTowards(MoveDirection direction)
+    {
+        return switch (direction)
+        {
+            case UP -> moveUp();
+            case DOWN -> moveDown();
+            case LEFT -> moveLeft();
+            case RIGHT -> moveRight();
+        };
+    }
+
+    private boolean moveUp()
+    {
+        Player player = PlayerManager.getInstance().getPlayer();
+        if(player.getCurrentRoom().hasTopRoom())
+        {
+            player.setCurrentRoom(player.getCurrentRoom().getTopRoom());
+            return true;
+        }
+        return false;
+    }
+    private boolean moveDown()
+    {
+        Player player = PlayerManager.getInstance().getPlayer();
+        if(player.getCurrentRoom().hasBottomRoom())
+        {
+            player.setCurrentRoom(player.getCurrentRoom().getBottomRoom());
+            return true;
+        }
+        return false;
+    }
+    private boolean moveLeft()
+    {
+        Player player = PlayerManager.getInstance().getPlayer();
+        if(player.getCurrentRoom().hasLeftRoom())
+        {
+            player.setCurrentRoom(player.getCurrentRoom().getLeftRoom());
+            return true;
+        }
+        return false;
+    }
+    private boolean moveRight()
+    {
+        Player player = PlayerManager.getInstance().getPlayer();
+        if(player.getCurrentRoom().hasRightRoom())
+        {
+            player.setCurrentRoom(player.getCurrentRoom().getRightRoom());
+            return true;
+        }
+        return false;
+    }
+
+    public static MovementManager getInstance()
+    {
+        return instance;
+    }
 }
--- src/de/prog2/dungeontop/model/game/Player.java
+++ src/de/prog2/dungeontop/model/game/Player.java
@@ -1,11 +1,14 @@
 package de.prog2.dungeontop.model.game;
 
+import de.prog2.dungeontop.model.world.Room;
 import de.prog2.dungeontop.utils.GlobalLogger;
 
 import de.prog2.dungeontop.resources.LoggerStringValues;
 
-public class Player{
+public class Player
+{
     private int souls;
+    private Room currentRoom;
 
     public Player (){
         this.souls = 0;
@@ -14,13 +17,22 @@ public class Player{
         this.souls = souls;
     }
 
+    /*-----------------------------------------GETTER AND SETTER------------------------------------------------------*/
     public int getSouls(){
         GlobalLogger.log(LoggerStringValues.PLAYERSOULS_GET);
         return this.souls;
     }
-
     public void setSouls(int souls){
         GlobalLogger.log(LoggerStringValues.PLAYERSOULS_SET + souls + LoggerStringValues.SET);
         this.souls = souls;
+    }
+
+    public Room getCurrentRoom()
+    {
+        return currentRoom;
+    }
+    public void setCurrentRoom(Room currentRoom)
+    {
+        this.currentRoom = currentRoom;
     }
 }
