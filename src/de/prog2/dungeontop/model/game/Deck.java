e257465910689462f9c0f356446027ef6d0dd334
jpranata <jpranata@stud.hs-bremen.de>
2022-05-14 17:37:30 +0200

    added a list for cards and methods to add and remove cards

--- src/de/prog2/dungeontop/model/game/Deck.java
+++ src/de/prog2/dungeontop/model/game/Deck.java
@@ -1,4 +1,30 @@
 package de.prog2.dungeontop.model.game;
 
+import java.util.ArrayList;
+
 public class Deck {
+    ArrayList<Card> deck;
+
+    public Deck(ArrayList<Card> deck)
+    {
+        this.deck = deck;
+    }
+
+    public ArrayList<Card> getDeck()
+    {
+        return deck;
+    }
+    public void setDeck(ArrayList<Card> deck)
+    {
+        this.deck = deck;
+    }
+    public void addCard(Card card)
+    {
+        deck.add(card);
+    }
+
+    public void removeCard(Card card)
+    {
+        deck.remove(card);
+    }
 }
