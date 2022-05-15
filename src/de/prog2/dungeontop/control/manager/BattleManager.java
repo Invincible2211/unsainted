ad47e18a2e85aea00f60f8a57c8a7f0d7c15b596
Jesse Gollub <jesse.gollub@ewe.net>
2022-05-15 21:38:49 +0200

    implemented EntityMovement and removed unnecessary methods in BattleManager.
     Possibly there are some errors from the latest merge.

--- src/de/prog2/dungeontop/control/manager/BattleManager.java
+++ src/de/prog2/dungeontop/control/manager/BattleManager.java
@@ -1,23 +1,30 @@
 package de.prog2.dungeontop.control.manager;
 
+import de.prog2.dungeontop.control.controller.EntityController;
 import de.prog2.dungeontop.model.entities.Entity;
+import de.prog2.dungeontop.model.game.Card;
 import de.prog2.dungeontop.model.game.DungeonMaster;
+import de.prog2.dungeontop.model.game.MoveDirection;
 import de.prog2.dungeontop.model.game.Player;
 import de.prog2.dungeontop.model.world.Coordinate;
 import de.prog2.dungeontop.model.world.arena.Arena;
 import de.prog2.dungeontop.resources.ExceptionMessagesKeys;
 import de.prog2.dungeontop.resources.LoggerStringValues;
+import de.prog2.dungeontop.utils.CoordinateDirections;
 import de.prog2.dungeontop.utils.GlobalLogger;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class BattleManager
 {
-
-    private Player player = null;
-    private DungeonMaster dm = null;
-    private Arena arena = null;
+    private Player player;
+    private DungeonMaster dm;
+    private Arena arena;
     private BattlePhase currentPhase = BattlePhase.START;
+    private Player firstduellist = null;
+    private Player secondduellist = null;
+    private Player currentActiveDuellist = null;
     private boolean isPlayerfirst = false;
 
 
@@ -34,88 +41,120 @@ public class BattleManager
     public void startBattle ()
     {
         GlobalLogger.log(LoggerStringValues.START_BATTLE);
-        if (ThreadLocalRandom.current().nextBoolean() == true){
+        Random random = new Random();
+        if (random.nextBoolean()) // true dann darf der spieler
+        {
             GlobalLogger.log(LoggerStringValues.PLAYER_CAN_CHOOSE_WHO_IS_FIRST);
             // HIER DARF SICH DER SPIELER AUSSUCHEN WER ZUERST ZIEHT... MIT EVENT?
+            //Wait for choice of another Thread when we are allowed to
+            boolean choice = true;
+            setPlayerAsFirstDuellist(choice); //TODO hier das TRUE aendern mit dem Ergebnis der choice wer zuerst darf
             setCurrentPhase(getNextPhaseInCycle());
-        } else {
-            GlobalLogger.log(LoggerStringValues.DM_CAN_CHOOSE_WHO_IS_FIRST);
-            // HIER DARF SICH DER DM AUSSUCHEN WER ZUERST ZIEHT... MIT EVENT?
-            //Wait for choice
-            setCurrentPhase(getNextPhaseInCycle());
+            return;
         }
+         // ansonsten darf der dm
+        GlobalLogger.log(LoggerStringValues.DM_CAN_CHOOSE_WHO_IS_FIRST);
+        // HIER DARF SICH DER DM AUSSUCHEN WER ZUERST ZIEHT... MIT EVENT?
+        //Wait for choice of another Thread when we are allowed to
+        boolean choice = true;
+        setPlayerAsFirstDuellist(choice); //TODO hier das TRUE aendern mit dem Ergebnis der choice wer zuerst darf
+        setCurrentPhase(getNextPhaseInCycle());
     }
 
 
     /**
-     * Legt die Reihnfolge des Battles fest
-     * Fuer die Moeglichkeit bei Phases mehr Logik zu implimentieren werden Phasen als Enum genommen anstatt extern in properties zu stehen.
+     * Legt die Reihenfolge des Battles fest
+     * Fuer die Moeglichkeit bei Phases mehr Logik zu implementieren werden Phasen als Enum genommen, anstatt extern in properties zu stehen.
      * @return die naechste Phase in einem Battle die wiederholbar ist. Kein Start und kein End
      */
     public BattlePhase getNextPhaseInCycle ()
     {
-        switch(this.getCurrentPhase()){
-            case START:
-            case SECOND_DUELLIST_SECOND_PLACE_CARDS:
+        switch (this.getCurrentPhase())
+        {
+            case START, SECOND_DUELLIST_SECOND_PLACE_CARDS ->
+            {
                 GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
                 return BattlePhase.DRAW_CARDS;
+
             case DRAW_CARDS:
                 GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
                 return BattlePhase.FIRST_DUELLIST_PLACE_CARDS;
-            case FIRST_DUELLIST_PLACE_CARDS:
+            }
+            case FIRST_DUELLIST_PLACE_CARDS ->
+            {
+                GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
+                return BattlePhase.SECOND_DUELLIST_DRAW;
+            }
+            case SECOND_DUELLIST_DRAW ->
+            {
                 GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
                 return BattlePhase.SECOND_DUELLIST_PLACE_CARDS;
-            case SECOND_DUELLIST_PLACE_CARDS:
+            }
+            case SECOND_DUELLIST_PLACE_CARDS ->
+            {
                 GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
                 return BattlePhase.FIRST_DUELLIST_MINION_ACT;
-            case FIRST_DUELLIST_MINION_ACT:
+            }
+            case FIRST_DUELLIST_MINION_ACT ->
+            {
                 GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
                 return BattlePhase.SECOND_DUELLIST_MINION_ACT;
-            case SECOND_DUELLIST_MINION_ACT:
+            }
+            case SECOND_DUELLIST_MINION_ACT ->
+            {
                 GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
                 return BattlePhase.FIRST_DUELLIST_SECOND_PLACE_CARDS;
-            case FIRST_DUELLIST_SECOND_PLACE_CARDS:
+            }
+            case FIRST_DUELLIST_SECOND_PLACE_CARDS ->
+            {
                 GlobalLogger.log(LoggerStringValues.GOT_NEXT_BATTLEPHASE);
                 return BattlePhase.SECOND_DUELLIST_SECOND_PLACE_CARDS;
-            case END:
+            }
+            case END ->
+            {
                 GlobalLogger.warning(LoggerStringValues.TRY_TO_GET_PHASE_AFTER_END);
                 return BattlePhase.END;
-            default:
+            }
+            default ->
+            {
                 GlobalLogger.warning(ExceptionMessagesKeys.CAN_NOT_FIND_BATTLESTATE);
                 return null;
+            }
         }
     }
 
     public void endBattle (boolean victory)
     {
         setCurrentPhase(BattlePhase.END);
+        if (victory)
+        {
+            GlobalLogger.log(LoggerStringValues.DUELL_HAS_ENDED_VICTORIOUS);
+            return;
+        }
+        GlobalLogger.log(LoggerStringValues.DUELL_HAS_ENDED_IN_DEFEAT);
     }
 
 
     /**
      * Places card on Arena tile,
      * @param player who controlls the card
-     * @param cordinate where to place new minion
+     * @param coordinate where to place new minion
      */
-    public void placeCard (Player player, Coordinate cordinate)
+    public void placeCard (Player player, Coordinate coordinate, Card card)
+    {
+                //remove from hand
+        //put in list of units who cant move
+        //put in list of units who cant attack
+    }
+
+
+
+
+    public void attack (Player owner, Entity attacker, Entity attacked)
     {
 
     }
 
-    public void placeCard (DungeonMaster dungeonMaster)
-    {
-
-    }
-
-    public void attack (Player player)
-    {
-
-    }
-
-    public void attack (DungeonMaster dungeonMaster)
-    {
-
-    }
 
     /**
      * End the Battle and gives Souls and Skulls
@@ -141,18 +180,24 @@ public class BattleManager
         updateStats(arena.getAllMinions());
     }
 
-    public void moveUnit (Entity mover, Coordinate coordinate)
+    public void moveUnit (Entity mover, MoveDirection direction)
     {
-
+        EntityController.tryMoveTowards(this.arena, mover, direction);
     }
 
-    public boolean isAllowedMove (Entity mover, Coordinate coordinate)
+    private boolean isInRangeToAttack (Entity attacker, Entity attacked)
     {
-        return false; // TODO
-    }
-    public void attackUnit (Entity attacker, Entity attecked)
-    {
-
+        Coordinate coordinateOfAttacker = attacker.getPosition();
+        for (Coordinate coordinate : CoordinateDirections.getStraightNeighbours(coordinateOfAttacker))
+        {
+            if (coordinate == attacked.getPosition())
+            {
+                GlobalLogger.log(LoggerStringValues.ENTITY_HAS_RANGE_TO_ATTACK);
+                return true;
+            }
+        }
+        GlobalLogger.log(LoggerStringValues.ENTIT_DOES_NOT_HAVE_TARGET_IN_RANGE);
+        return false;
     }
 
     /**
@@ -163,6 +208,7 @@ public class BattleManager
         START, //at threading maybe wait for connections, Decide Beginner
         DRAW_CARDS, //where duellists get to draw to handcardlimit
         FIRST_DUELLIST_PLACE_CARDS, //first duellist can place cards on arena
+        SECOND_DUELLIST_DRAW, //where second duellist draws cards
         SECOND_DUELLIST_PLACE_CARDS, //second duellist can place cards on arena
         FIRST_DUELLIST_MINION_ACT, // first duellist can move and attack minions on field
         SECOND_DUELLIST_MINION_ACT, // second duellist can move and attack minions on field
@@ -180,6 +226,7 @@ public class BattleManager
         END // choose treasure, get exp, skulls, etc. and go back to overworld
     }
 
+    /*---------------------------------------------- GETTER UND SETTER------------------------------------------------*/
 
     public Player getPlayer ()
     {
@@ -221,6 +268,140 @@ public class BattleManager
         this.currentPhase = currentPhase;
     }
 
+    public ArrayList<Card> getFirstDuellistHand ()
+    {
+        return firstDuellistHand;
+    }
+
+    public void setFirstDuellistHand (ArrayList<Card> firstDuellistHand)
+    {
+        this.firstDuellistHand = firstDuellistHand;
+    }
+
+    public ArrayList<Card> getSecondDuellistHand ()
+    {
+        return secondDuellistHand;
+    }
+
+    public void setSecondDuellistHand (ArrayList<Card> secondDuellistHand)
+    {
+        this.secondDuellistHand = secondDuellistHand;
+    }
+
+    public ArrayList<Entity> getFirstDuellistEntitiesWhoCanAttack ()
+    {
+        return firstDuellistEntitiesWhoCanAttack;
+    }
+
+    public void setFirstDuellistEntitiesWhoCanAttack (ArrayList<Entity> firstDuellistEntitiesWhoCanAttack)
+    {
+        this.firstDuellistEntitiesWhoCanAttack = firstDuellistEntitiesWhoCanAttack;
+    }
+
+    public ArrayList<Entity> getSecondDuellistEntitiesWhoCanAttack ()
+    {
+        return secondDuellistEntitiesWhoCanAttack;
+    }
+
+    public void setSecondDuellistEntitiesWhoCanAttack (ArrayList<Entity> secondDuellistEntitiesWhoCanAttack)
+    {
+        this.secondDuellistEntitiesWhoCanAttack = secondDuellistEntitiesWhoCanAttack;
+    }
+
+    public ArrayList<Entity> getFirstDuellistEntitiesWhoCanMove ()
+    {
+        return firstDuellistEntitiesWhoCanMove;
+    }
+
+    public void setFirstDuellistEntitiesWhoCanMove (ArrayList<Entity> firstDuellistEntitiesWhoCanMove)
+    {
+        this.firstDuellistEntitiesWhoCanMove = firstDuellistEntitiesWhoCanMove;
+    }
+
+    public ArrayList<Entity> getSecondDuellistEntitiesWhoCanMove ()
+    {
+        return secondDuellistEntitiesWhoCanMove;
+    }
+
+    public void setSecondDuellistEntitiesWhoCanMove (ArrayList<Entity> secondDuellistEntitiesWhoCanMove)
+    {
+        this.secondDuellistEntitiesWhoCanMove = secondDuellistEntitiesWhoCanMove;
+    }
+
+    public Player getFirstduellist ()
+    {
+        return firstduellist;
+    }
+
+    public void setFirstduellist (Player firstduellist)
+    {
+        this.firstduellist = firstduellist;
+    }
+
+    public Player getSecondduellist ()
+    {
+        return secondduellist;
+    }
+
+    public void setSecondduellist (Player secondduellist)
+    {
+        this.secondduellist = secondduellist;
+    }
+
+    private void setCurrentActiveDuellist (Player player)
+    {
+        this.currentActiveDuellist = player;
+    }
+
+    private Player getCurrentActiveDuellist ()
+    {
+        if (this.currentActiveDuellist == this.firstduellist)
+        {
+            return this.firstduellist;
+        }
+        return this.secondduellist;
+    }
+
+    private ArrayList<Card> getCurrentHand ()
+    {
+        if (this.currentActiveDuellist == this.firstduellist)
+        {
+            return this.getFirstDuellistHand();
+        }
+        return this.getSecondDuellistHand();
+    }
+
+    private ArrayList<Entity> getCurrentEntitiesWhoCanAttack ()
+    {
+        if (this.currentActiveDuellist == this.firstduellist)
+        {
+            return this.getFirstDuellistEntitiesWhoCanAttack();
+        }
+        return this.getSecondDuellistEntitiesWhoCanAttack();
+    }
+
+    private ArrayList<Entity> getCurrentEntitiesWhoCanMove ()
+    {
+        if (this.currentActiveDuellist == this.firstduellist)
+        {
+            return this.getFirstDuellistEntitiesWhoCanMove();
+        }
+        return this.getSecondDuellistEntitiesWhoCanMove();
+    }
+
+    private void setPlayerAsFirstDuellist (boolean choice)
+    {
+        if (choice)
+        {
+            this.firstduellist = this.player;
+            this.secondduellist = this.dm;
+            GlobalLogger.log(LoggerStringValues.PLAYER_GOES_FIRST);
+            return;
+        }
+        this.firstduellist = this.dm;
+        this.secondduellist = this.player;
+        GlobalLogger.log(LoggerStringValues.DM_GOES_FIRST);
+    }
     public boolean isPlayerfirst ()
     {
         return isPlayerfirst;
--- src/de/prog2/dungeontop/model/entities/Entity.java
+++ src/de/prog2/dungeontop/model/entities/Entity.java
@@ -1,5 +1,6 @@
 package de.prog2.dungeontop.model.entities;
 
+import de.prog2.dungeontop.model.game.Player;
 import de.prog2.dungeontop.model.world.Coordinate;
 
 public abstract class Entity
@@ -9,13 +10,19 @@ public abstract class Entity
     private int attackDamage = 0;
     private int movement = 0;
     private Coordinate position;
+    private int movesLeft = 0, maxMoves = 0;
+    private int attackRange = 0, maxAttackRange = 0;
+    private Player owner = null;
 
     /*--------------------------------------------CONSTRUCTORS--------------------------------------------------------*/
-    public Entity(int hp, int attackDamage, int movement)
+    public Entity(int hp, int attackDamage, int movement, int maxMoves, Player owner)
     {
         this.hp = hp;
         this.attackDamage = attackDamage;
         this.movement = movement;
+        this.maxMoves = maxMoves;
+        this.movesLeft = maxMoves;
+        this.owner = owner;
     }
 
     /*-----------------------------------------GETTER AND SETTER------------------------------------------------------*/
@@ -57,5 +64,54 @@ public abstract class Entity
     public void setPosition(Coordinate position)
     {
         this.position = position;
+    }
+
+    public int getMaxMoves()
+    {
+        return maxMoves;
+    }
+
+    public void setMaxMoves(int maxMoves)
+    {
+        this.maxMoves = maxMoves;
+    }
+
+    public int getMovesLeft()
+    {
+        return movesLeft;
+    }
+    public void setMovesLeft(int movesLeft)
+    {
+        this.movesLeft = movesLeft;
+    }
+
+    public int getAttackRange()
+    {
+        return attackRange;
+    }
+
+    public void setAttackRange(int attackRange)
+    {
+        this.attackRange = attackRange;
+    }
+
+    public int getMaxAttackRange()
+    {
+        return maxAttackRange;
+    }
+
+    public void setMaxAttackRange(int maxAttackRange)
+    {
+        this.maxAttackRange = maxAttackRange;
+    }
+
+    public Player getOwner()
+    {
+        return owner;
+    }
+
+    public void setOwner(Player owner)
+    {
+        this.owner = owner;
     }
 }
