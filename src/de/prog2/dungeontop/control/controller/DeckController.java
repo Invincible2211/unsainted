95e121a2e0fa3494eb0aed69ae6c97c2623a1523
Jesse Gollub <jesse.gollub@ewe.net>
2022-05-15 15:17:33 +0200

    added functionalities to DeckController

--- src/de/prog2/dungeontop/control/controller/DeckController.java
+++ src/de/prog2/dungeontop/control/controller/DeckController.java
@@ -1,5 +1,6 @@
 package de.prog2.dungeontop.control.controller;
 
+import de.prog2.dungeontop.model.game.Card;
 import de.prog2.dungeontop.model.game.Deck;
 import java.util.Collections;
 
@@ -11,5 +12,21 @@ public class DeckController
     public static void shuffleDeck(Deck deck)
     {
         Collections.shuffle(deck.getDeck());
+    }
+
+    /**
+     * Draw the first card on the deck
+     */
+    public static Card drawCard(Deck deck)
+    {
+        return deck.popCard();
+    }
+
+    /**
+     * Add a card to the deck
+     */
+    public static void pushCard(Deck deck, Card card)
+    {
+        deck.pushCard(card);
     }
 }
--- src/de/prog2/dungeontop/model/game/Deck.java
+++ src/de/prog2/dungeontop/model/game/Deck.java
@@ -1,23 +1,32 @@
 package de.prog2.dungeontop.model.game;
 
-import java.util.LinkedList;
+import java.util.Stack;
 
-public class Deck {
-    LinkedList<Card> deck;
+public class Deck
+{
+    Stack<Card> deck;
 
     public Deck()
     {
-        this.deck = new LinkedList<>();
+        this.deck = new Stack<>();
+    }
+    public Deck(Stack<Card> deck)
+    {
+        this.deck = deck;
     }
 
-    public void addCard(Card card)
+    public void pushCard(Card card)
     {
-        deck.add(card);
+        deck.push(card);
     }
 
     public void removeCard(Card card)
     {
         deck.remove(card);
+    }
+    public Card popCard()
+    {
+        return deck.pop();
     }
 
     public boolean containsCard(Card card)
@@ -26,7 +35,7 @@ public class Deck {
     }
 
     //Set- and Getters
-    public LinkedList<Card> getDeck()
+    public Stack<Card> getDeck()
     {
         return deck;
     }
