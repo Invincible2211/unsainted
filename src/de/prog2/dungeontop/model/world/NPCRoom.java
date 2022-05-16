49612fdc2408c58eee6d841ec0d460b780ac2042
Thomas K <ttt2007@hotmail.de>
2022-05-16 18:02:40 +0200

    extended NpcRoom class

--- src/de/prog2/dungeontop/model/world/NPCRoom.java
+++ src/de/prog2/dungeontop/model/world/NPCRoom.java
@@ -5,5 +5,15 @@ package de.prog2.dungeontop.model.world;
  */
 public abstract class NPCRoom extends Room
 {
-    // nte Aktion kostenlos ...
+    private int freeActions = 1;
+    public NPCRoom () {}
+    public NPCRoom (int freeActions)
+    {
+        this.freeActions = freeActions;
+    }
+
+    public int getFreeActions () { return this.freeActions; }
+    public void setFreeActions (int freeActions) { this.freeActions = freeActions; }
+    public void decrementFreeActions () { this.freeActions++; }
+    public void increaseFreeActions (int increase) { this.setFreeActions(this.getFreeActions() + increase);}
 }
