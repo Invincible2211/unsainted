312a3ec73ebcd859d50c63a229d12d2d1d46abf5
Jason <jasonpranata01@gmail.com>
2022-05-17 21:05:15 +0200

    Test- Talent, Spell and Skills and their value interfaces added

--- src/de/prog2/dungeontop/resources/StringValues.java
+++ src/de/prog2/dungeontop/resources/StringValues.java
@@ -3,4 +3,7 @@ package de.prog2.dungeontop.resources;
 public interface StringValues
 {
     String FILE_TYPE = "properties";
+
+    String TEST_TALENT_NAME = "Specialist";
+    String TEST_TALENT_DESCRIPTION = "Hat dauerhaft plus 2 in einem Perk.";
 }
