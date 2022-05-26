3f74f1c3cbd421c8963176fdce4dbac0a61a0d18
FynnK <Fynn.kwast@btc-ag.com>
2022-05-26 18:01:04 +0200

    added commentary for futurePast functionality
    For the logik that we classified as optional

--- src/de/prog2/dungeontop/model/skills/ManaPool.java
+++ src/de/prog2/dungeontop/model/skills/ManaPool.java
@@ -1,5 +1,5 @@
 package de.prog2.dungeontop.model.skills;
-
+//Will only be implemented when we implement furter battlelogik. Optional fuer die Abgabe
 public class ManaPool
 {
     private int currentMana = 0, maxMana = 0;
--- src/de/prog2/dungeontop/model/skills/PassiveSkill.java
+++ src/de/prog2/dungeontop/model/skills/PassiveSkill.java
@@ -1,5 +1,5 @@
 package de.prog2.dungeontop.model.skills;
-
+//Will only be implemented when we implement furter battlelogik. Optional fuer die Abgabe
 public abstract class PassiveSkill extends Skill
 {
     public PassiveSkill(String name, String description, int range) {
