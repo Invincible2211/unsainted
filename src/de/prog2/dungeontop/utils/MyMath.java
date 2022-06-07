5716621bdf767b0c0e60521a7f6faf3f5c39e57e
Jesse Gollub <jesse.gollub@ewe.net>
2022-06-07 16:11:36 +0200

    fixup! added MyMath for central location for general math

--- src/de/prog2/dungeontop/utils/MyMath.java
+++ src/de/prog2/dungeontop/utils/MyMath.java
@@ -6,4 +6,9 @@ public abstract class MyMath
     {
         return Math.max(min, Math.min(max, value));
     }
+    public static <T extends Comparable<T>> T clamp(T val, T min, T max) {
+        if (val.compareTo(min) < 0) return min;
+        else if (val.compareTo(max) > 0) return max;
+        else return val;
+    }
 }
