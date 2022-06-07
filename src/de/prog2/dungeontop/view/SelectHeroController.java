17477faf5c0c9f807d8a4e9758b116a68a9e3b07
Jason <jasonpranata01@gmail.com>
2022-06-07 13:27:45 +0200

    Controller for Hero Select Screen.

--- src/de/prog2/dungeontop/view/SelectHeroController.java
+++ src/de/prog2/dungeontop/view/SelectHeroController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -15,73 +14,50 @@ import java.io.IOException;
 
 public class SelectHeroController
 {
+    private final FXMLLoader fxmlLoader = new FXMLLoader();
+    private Stage heroScreenStage = new Stage();
 
-    @FXML
-    private VBox heroText1;
-
-    @FXML
-    private VBox heroText2;
-
-    @FXML
-    private VBox heroText3;
-
-
-    @FXML
-    private void onHero1ButtonEntered()
+    public SelectHeroController()
     {
-        heroText1.setVisible(true);
-    }
-    @FXML
-    private void onHero2ButtonEntered()
-    {
-        heroText2.setVisible(true);
-    }
-    @FXML
-    private void onHero3ButtonEntered()
-    {
-        heroText3.setVisible(true);
-    }
-
-    @FXML
-    private void onHero1ButtonExit()
-    {
-        heroText1.setVisible(false);
-    }
-
-    @FXML
-    private void onHero2ButtonExit()
-    {
-        heroText2.setVisible(false);
-    }
-    @FXML
-    private void onHero3ButtonExit()
-    {
-        heroText3.setVisible(false);
+        heroScreenStage.initModality(Modality.APPLICATION_MODAL);
+        heroScreenStage.initOwner(DungeonTop.getStage());
+        heroScreenStage.initStyle(StageStyle.UNDECORATED);
+        AnchorPane root = new AnchorPane();
+        try {
+            root = fxmlLoader.load(DungeonTop.class.getClassLoader().getResourceAsStream(ViewStrings.HERO_SCREEN_FXML));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene heroScreenScene = new Scene(root);
+        heroScreenStage.setScene(heroScreenScene);
     }
 
     @FXML
     private void onHero1ButtonPressed()
     {
 
+        heroScreenStage.show();
     }
 
     @FXML
     private void onHero2ButtonPressed()
     {
 
-
+        heroScreenStage.show();
     }
 
     @FXML
     private void onHero3ButtonPressed()
     {
 
-
+        heroScreenStage.show();
     }
 
-    private void onConfirmButtonPressed()
+    @FXML
+    private void onHero4ButtonPressed()
     {
 
+        heroScreenStage.show();
     }
 
     @FXML
