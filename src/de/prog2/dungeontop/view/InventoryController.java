7bae35d46454ce352688c520b759dc983f063071
Jason <jasonpranata01@gmail.com>
2022-06-11 20:46:33 +0200

    initialized buttons and added some empty methods(to be filled) for said buttons

--- src/de/prog2/dungeontop/view/InventoryController.java
+++ src/de/prog2/dungeontop/view/InventoryController.java
@@ -1,6 +1,34 @@
 package de.prog2.dungeontop.view;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class InventoryController
 {
+
+    @FXML
+    private Button useItemButton;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private void onReturnButtonClicked()
+    {
+
+    }
+
+    @FXML
+    private void onUseItemButtonClicked()
+    {
+
+    }
+
+    @FXML
+    private void onItemButtonClicked()
+    {
+
+    }
 
 }
