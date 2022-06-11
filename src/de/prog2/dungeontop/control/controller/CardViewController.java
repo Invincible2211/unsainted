b9086e29d5375ca06df76866ee28d5deb7dc089f
Jesse Gollub <jesse.gollub@ewe.net>
2022-06-11 15:33:10 +0200

    fixed cardView

--- src/de/prog2/dungeontop/control/controller/CardViewController.java
+++ src/de/prog2/dungeontop/control/controller/CardViewController.java
@@ -2,6 +2,7 @@ package de.prog2.dungeontop.control.controller;
 
 import de.prog2.dungeontop.DungeonTop;
 import de.prog2.dungeontop.model.game.Card;
+import de.prog2.dungeontop.resources.StringValues;
 import de.prog2.dungeontop.view.CardView;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -27,7 +28,8 @@ public abstract class CardViewController
 
     private static void fillCardViewWithData(Card card, CardView controller)
     {
-        controller.getRankLabel().setText(String.valueOf(card.getRank()));
+        controller.getRankLabel().setText(String.format(StringValues.RANK, card.getRank(), card.getMaxRank()));
+        controller.getSummonCostLabel().setText(String.format(StringValues.SUMMON_COST, card.getSummonCost()));
         controller.setCard(card);
     }
 }
