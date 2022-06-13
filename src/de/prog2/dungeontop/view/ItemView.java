5b5082dbee265a4f9830f3ac916af932d770175b
Jason <jasonpranata01@gmail.com>
2022-06-13 23:39:36 +0200

    made itemview and its controller

--- src/de/prog2/dungeontop/view/ItemView.java
+++ src/de/prog2/dungeontop/view/ItemView.java
@@ -1,6 +1,38 @@
 package de.prog2.dungeontop.view;
 
+import de.prog2.dungeontop.model.items.Item;
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+
 public class ItemView
 {
+    @FXML
+    Label itemName;
 
+    @FXML
+    ImageView itemImage;
+
+    private Item item;
+
+    public Label getItemName()
+    {
+        return itemName;
+    }
+
+    public ImageView getItemImage()
+    {
+        return itemImage;
+    }
+
+    public Item getItem()
+    {
+        return item;
+    }
+
+    public void setItem(Item item)
+    {
+        this.item = item;
+    }
 }
