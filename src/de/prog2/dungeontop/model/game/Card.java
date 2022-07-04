c1d4d1dd6ca5aa714741d199dc81c2bab3d1fb68
Jesse Gollub <jesse.gollub@ewe.net>
2022-07-04 15:44:38 +0200

    changed cards to no longer have assets

--- src/de/prog2/dungeontop/model/game/Card.java
+++ src/de/prog2/dungeontop/model/game/Card.java
@@ -12,30 +12,14 @@ public abstract class Card
     private int price;
     private final int maxRank;
     private int summonCost;
-    private final HashMap<AssetType, Integer> assetTypeHashMap = new HashMap<>();
 
-    public enum AssetType
-    {
-        A,B,C
-    }
-
-    public Card(int maxRank, int price, int rank, int summonCost, List... assets)
+    public Card(int maxRank, int price, int rank, int summonCost)
     {
         GlobalLogger.log(LoggerStringValues.CARD_CREATED);
         this.maxRank = maxRank;
         this.price = price;
         this.rank = rank;
         this.summonCost = summonCost;
-
-        for (var asset : assets)
-        {
-            assetTypeHashMap.put((AssetType) asset.get(0), (Integer) asset.get(1));
-        }
-    }
-
-    public int getAssetId (AssetType type)
-    {
-        return assetTypeHashMap.get(type);
     }
 
     public int getRank()
--- src/de/prog2/dungeontop/model/spells/Spell.java
+++ src/de/prog2/dungeontop/model/spells/Spell.java
@@ -2,32 +2,21 @@ package de.prog2.dungeontop.model.spells;
 
 public abstract class Spell
 {
-    private String name;
-    private int ego_points;
+    private final String name;
+    private final int assetId;
 
-    public Spell (String name, int ego_points)
+    public Spell (String name, int assetId)
     {
         this.name = name;
-        this.ego_points = ego_points;
+        this.assetId = assetId;
     }
 
-    //Set- and Getters
     public String getName()
     {
         return name;
     }
-    public void setName(String name)
+    public int getAssetId()
     {
-        this.name = name;
-    }
-
-    public int getEgo_points()
-    {
-        return ego_points;
-    }
-
-    public void setEgo_points(int ego_points)
-    {
-        this.ego_points = ego_points;
+        return assetId;
     }
 }
