6f6fa6a70bd1c0b65d156e689ddc740c01e43fd0
Freddyblitz <fynn.schur@web.de>
2022-07-06 13:01:54 +0200

    fixed stuff

--- src/de/prog2/dungeontop/model/world/actions/Action.java
+++ src/de/prog2/dungeontop/model/world/actions/Action.java
@@ -1,9 +1,11 @@
 package de.prog2.dungeontop.model.world.actions;
 
+import java.io.Serializable;
+
 /**
  * Action Interface. It exists for all the different Room-Actions.
  */
-public interface Action
+public interface Action extends Serializable
 {
     <T>boolean execute(T... args);
 }
