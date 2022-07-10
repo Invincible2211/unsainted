7218725374c6114b381671f7244ae6372b871ac7
TurboThommy <ttt2007@hotmail.de>
2022-07-10 18:12:49 +0200

    things

--- src/de/prog2/dungeontop/resources/ApplicationConstants.java
+++ src/de/prog2/dungeontop/resources/ApplicationConstants.java
@@ -3,7 +3,7 @@ package de.prog2.dungeontop.resources;
 import de.prog2.dungeontop.utils.GlobalLogger;
 
 public interface ApplicationConstants {
-
+    GlobalLogger.LoggerLevel LOGGER_LEVEL = GlobalLogger.LoggerLevel.DEFAULT;
     int RESOLUTION_X = 1920;
     int RESOLUTION_Y = 1080;
 }
