224a71e6fa5c0874f94520a796a2dde5c45322f9
Freddyblitz <fynn.schur@web.de>
2022-07-10 18:48:39 +0200

    open Arena DM

--- src/de/prog2/dungeontop/control/network/NetworkInterpreter.java
+++ src/de/prog2/dungeontop/control/network/NetworkInterpreter.java
@@ -1,10 +1,12 @@
 package de.prog2.dungeontop.control.network;
 
 import de.prog2.dungeontop.DungeonTop;
+import de.prog2.dungeontop.control.manager.BattleManager;
 import de.prog2.dungeontop.control.manager.GameManager;
 import de.prog2.dungeontop.control.manager.PlayerManager;
 import de.prog2.dungeontop.model.network.Package;
 import de.prog2.dungeontop.model.network.packages.HellPackage;
+import de.prog2.dungeontop.model.network.packages.OpenArenaPackage;
 import de.prog2.dungeontop.model.network.packages.PlayerMovementPackage;
 import de.prog2.dungeontop.model.network.packages.PlayerPackage;
 import de.prog2.dungeontop.model.world.Coordinate;
@@ -78,6 +80,8 @@ public class NetworkInterpreter extends Thread{
             PlayerPackage playerPackage = (PlayerPackage) dataPackage;
             GameManager.getInstance().setOpponentPlayer(playerPackage.getPlayer());
             System.out.println(GameManager.getInstance().getOpponentPlayer().getEgo_points());
+        } else if (dataPackage instanceof OpenArenaPackage){
+            GameManager.getInstance().beginBattle();
         }
     }
 
