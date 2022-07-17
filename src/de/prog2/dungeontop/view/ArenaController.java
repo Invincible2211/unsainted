9fc32fecef05dc3b6af1731da58d77980de511fb
FynnK <Fynn.kwast@btc-ag.com>
2022-07-18 01:48:12 +0200

    KONZENTRATION AB 1:30 NACHTS IST STARK

--- src/de/prog2/dungeontop/view/ArenaController.java
+++ src/de/prog2/dungeontop/view/ArenaController.java
@@ -523,6 +523,7 @@ public class ArenaController
     public Label getLabelEgoPointOpponent() {
         return labelEgoPointOpponent;
     }
+
     public AnchorPane getCardDetailViewContainer() {
         return cardView;
     }
@@ -531,4 +532,17 @@ public class ArenaController
         return nextPhaseButton;
     }
 
+    public void clearField ()
+    {
+        for (Node node : arenaGridPane.getChildren()) {
+            if (node instanceof AnchorPane){
+                AnchorPane pane = (AnchorPane) node;
+                pane.setStyle("-fx-background-color: none;");
+            }
+        }
+        currentArena.getArenaHashmap().clear();
+        currentArena.getOpponent().clear();
+        currentArena.getFriendly().clear();
+        GameManager.getInstance().getOpponentPlayer().getHero().setHp(GameManager.getInstance().getOpponentPlayer().getHero().getCard().getEntity().getHp());
+    }
 }
